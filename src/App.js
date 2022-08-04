f0a841fde2bab12b9143255c48b8a77aa810b8a9
Kong Yue Chen <kongyue.chen@gmail.com>
2022-08-04 14:20:25 -0400

    Starting Card

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import FeedbackItem from './components/FeedbackItem'
-import Header from './components/Header'
 import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
-  const [feedback, setFeedback] = useState([])
+  const [feedback, setFeedback] = useState(FeedbackData)
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
-
   )
 }
-
 export default App
